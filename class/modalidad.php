83ed10124795b831831ae4d63bdf7dfb10ff0000
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2013-11-06 14:38:53 -0400

    Eliminar Cosas Inecesarias

--- class/modalidad.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once("bd.php");
-class modalidad extends bd{
-	var $tabla="modalidad";
-}
-?>
--- class/sindicato.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once("bd.php");
-class sindicato extends bd{
-	var $tabla="sindicato";
-	
-}
-?>
