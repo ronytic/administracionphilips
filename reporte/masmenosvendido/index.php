e85c766e96c84a6049e0f5acc6022e5a8d85f57e
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2013-12-09 12:08:54 -0400

    Agragado reporte mas vendido

---
+++ reporte/masmenosvendido/index.php
@@ -0,0 +1,45 @@
+<?php
+include_once("../../login/check.php");
+$titulo="Reporte Producto Mas y Menos Vendido";
+$folder="../../";
+include_once("../../funciones/funciones.php");
+
+include_once("../../class/producto.php");
+$producto=new producto;
+$prod=todolista($producto->mostrarTodos("","nombre"),"codproducto","nombre,codbarra","-");
+
+$valor=array("mas"=>"Mas","menos"=>"Menos");
+
+include_once "../../cabecerahtml.php";
+$datos=todolista($usuarios->mostrarTodo("nivel!=1","paterno,materno,nombre"),"codusuarios","paterno,materno,nombre");
+?>
+<?php include_once "../../cabecera.php";?>
+<div class="grid_12">
+	<div class="contenido imagenfondo">
+    	<div class="grid_8 prefix_2 alpha">
+        	<fieldset>
+        	<div class="titulo"><?php echo $titulo;?></div>
+            <form id="busqueda" action="busqueda.php" method="post">
+                <table class="tablabus">
+                    <tr>
+                       <!-- <td colspan="4"><?php campos("Producto","codproducto","select",$prod,0)?></td>-->
+                        <td colspan="4" width="300"><?php campos("Vendedor","id","select",$datos,0)?></td>
+                        <td colspan="4" width="300"><?php campos("Productos","orden","select",$valor,0,"","mas")?>Vendido</td>
+                    </tr>
+                    <tr>
+                        <td><?php campos("Fecha de Inicio","fechainicio","date","")?></td>
+                        <td><?php campos("Fecha Fin","fechafin","date","")?></td>
+                        <?php /*<td><?php campos("Producto Existente","existente","select",array("0"=>"No","1"=>"Si"))?></td>*/?>
+                    </tr>
+                    <tr>
+                        <td><?php campos("Ver Reporte","enviar","submit","",0,array("size"=>15));?></td>
+                    </tr>
+                </table>
+            </form>
+            </fieldset>
+        </div>
+        <div class="clear"></div>
+        <div id="respuesta"></div>
+    </div>
+</div>
+<?php include_once "../../piepagina.php";?>
---
+++ reporte/masmenosvendido/ver.php
@@ -0,0 +1,101 @@
+<?php
+include_once("../../login/check.php");
+include_once("../../impresion/pdf.php");
+$titulo="Reporte de Venta de Productos";
+extract($_GET);
+class PDF extends PPDF{
+	function Cabecera(){
+		global $fechainicio,$fechafin;
+		if($fechainicio!=""){
+		$this->CuadroCabecera(30,"Fecha de Inicio:",20,fecha2Str($fechainicio));
+		}
+		if($fechafin!=""){
+		$this->CuadroCabecera(30,"Fecha Fin:",20,fecha2Str($fechafin));
+		}
+		$this->Ln();
+		$this->TituloCabecera(10,"N");
+		$this->TituloCabecera(80,"Nombre Producto");
+		$this->TituloCabecera(20,"Cant");
+		$this->TituloCabecera(20,"PrecUni");
+		$this->TituloCabecera(20,"Total");
+		//$this->TituloCabecera(20,"CantStock");
+		$this->TituloCabecera(20,"FechaVen");
+		$this->TituloCabecera(50,"Cliente");
+		$this->TituloCabecera(20,"C.I.");
+		$this->TituloCabecera(60,"Observación");
+	}	
+}
+
+
+$codproducto=$codproducto!=""?$codproducto:"%";
+
+$id=$id!=""?$id:"%";
+
+$existente=$existente=="1"?'and cantidadstock>0':'';
+if($fechainicio!="" && $fechafin!=""){
+	$fechainicio=$fechainicio!=""?$fechainicio:"%";
+	$fechafin=$fechafin!=""?$fechafin:"%";
+	$fechas=" and  (fecha BETWEEN '$fechainicio' and '$fechafin')";
+}
+include_once("../../class/producto.php");
+include_once("../../class/ventadetalle.php");
+include_once("../../class/venta.php");
+
+$ventadetalle=new ventadetalle;
+$venta=new venta;
+$producto=new producto;
+$where="codproducto LIKE '$codproducto' $fechas  $existente and id LIKE '$id'";
+/*if(!empty($fechacontrato)){
+	$where="`fechacontrato`<='$fechacontrato'";
+}
+if(!empty($codobra)){
+	$where=(empty($fechacontrato))?"`codobra`=$codobra":$where." and `codobra`=$codobra";
+}
+if(!empty($tipocontrato)){
+	$where=(empty($where))?$where."`tipocontrato` LIKE '%$tipocontrato%'":$where." and `tipocontrato` LIKE '%$tipocontrato%'";
+}*/
+
+//echo $where;
+$pdf=new PDF("L","mm","legal");
+$pdf->AddPage();
+$totales=array();
+$cantidadt=0;
+$preciot=0;
+$totalt=0;
+$cantidadstock=0;
+foreach($ventadetalle->mostrarTodos($where,"fecha") as $inv){$i++;
+	$ven=array_shift($venta->mostrar($inv['codventa']));
+	$cantidadt+=$inv['cantidad'];
+	$preciot+=$inv['preciounitario'];
+	$totalt+=$inv['subtotal'];
+	$cantidadstock+=$inv['cantidadstock'];
+
+	$pro=array_shift($producto->mostrar($inv['codproducto']));
+	
+	
+	$pdf->CuadroCuerpo(10,$i,0,"R");
+	$pdf->CuadroCuerpo(80,$pro['nombre'],0,"");
+	$pdf->CuadroCuerpo(20,($inv['cantidad']),1,"R",1);
+	$pdf->CuadroCuerpo(20,($inv['preciounitario']),1,"R",1);
+	$pdf->CuadroCuerpo(20,($inv['subtotal']),1,"R",1);
+	//$pdf->CuadroCuerpo(20,($inv['cantidadstock']),1,"R",1);
+	$pdf->CuadroCuerpo(20,fecha2Str($inv['fecha']),0,"",1);
+	$pdf->CuadroCuerpo(50,($ven['cliente']),0,"",1);
+	$pdf->CuadroCuerpo(20,($ven['ci']),0,"",1);
+	$pdf->CuadroCuerpo(60,($inv['observacion']),0,"L",1);
+	
+	$pdf->ln();
+}
+$pdf->Linea();
+$pdf->CuadroCuerpoResaltar(90,"Totales",1,"R",0);
+$pdf->CuadroCuerpoResaltar(20,$cantidadt,1,"R",1);
+$pdf->CuadroCuerpoResaltar(20,$preciot,1,"R",1);
+$pdf->CuadroCuerpoResaltar(20,$totalt,1,"R",1);
+//$pdf->CuadroCuerpoResaltar(20,$cantidadstock,1,"R",1);
+$pdf->CuadroCuerpoResaltar(55,"",0,"");
+//print_r($totales);
+
+$pdf->ln();$pdf->ln();$pdf->ln();$pdf->ln();$pdf->ln();$pdf->ln();
+
+$pdf->Output();
+?>
