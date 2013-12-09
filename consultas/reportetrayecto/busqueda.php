043079d18b22e2779e6e4f617458ae618b3a0780
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2013-12-09 12:08:45 -0400

    Eliminado Cosas menos importantes

--- consultas/reportetrayecto/busqueda.php
+++
@@ -1,9 +0,0 @@
-<?php
-$datos=array();
-foreach($_POST as $k=>$v){
-	array_push($datos,$k."=".$v);
-}
-
-$datos=implode("&",$datos);
-?>
-<iframe src="ver.php?<?php echo $datos;?>" width="100%" height="800"></iframe>
