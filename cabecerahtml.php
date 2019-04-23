1605733525b4cbc2aa8dd9fd8d89f638af4a574d
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-04-23 12:51:24 -0400

    Update cabecerahtml.php

--- cabecerahtml.php
+++ cabecerahtml.php
@@ -3,7 +3,8 @@ include_once 'class/usuarios.php';
 $codusuario=$_SESSION['idusuario'];
 $nivel=$_SESSION['nivel'];
 $usuarios=new usuarios;
-$us=array_shift($usuarios->mostrar($_SESSION['idusuario']));
+$us=$usuarios->mostrar($_SESSION['idusuario']);
+$us=array_shift($us);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php php_start();?>
@@ -11,14 +12,16 @@ $us=array_shift($usuarios->mostrar($_SESSION['idusuario']));
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="<?php php_start();?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php echo $titulo;?> | <?php echo $title?></title>
-<link href="<?php echo $folder;?>css/960/960.css" type="text/css" rel="stylesheet" media="screen">
+<link href="<?php echo $folder;?>css/bootstrap/bootstrap.min.css" type="text/css" rel="stylesheet" media="screen">
 <link href="<?php echo $folder;?>css/tabcontent.css" rel="stylesheet" type="text/css"/>
 <link href="<?php echo $folder;?>css/core.css" type="text/css" rel="stylesheet" media="screen">
 <link href="<?php echo $folder;?>css/menu/styles.css" type="text/css" rel="stylesheet" media="screen">
 <link href="<?php echo $folder;?>css/select2.css" type="text/css" rel="stylesheet">
 <link rel="shortcut icon" type="image/x-icon" href="<?php echo $folder; ?>imagenes/favicon.ico" />
-<script language="javascript" type="text/javascript" src="<?php echo $folder;?>js/jquery.js"></script>
+<script language="javascript" type="text/javascript" src="<?php echo $folder;?>js/jquery-1.12.4.min.js"></script>
+<script language="javascript" type="text/javascript" src="<?php echo $folder;?>js/bootstrap/bootstrap.min.js"></script>
 <script language="javascript" type="text/javascript" src="<?php echo $folder;?>js/tabcontent.js"></script> 
 <script language="javascript" type="text/javascript" src="<?php echo $folder;?>js/jquery.form.js"></script>
 <script language="javascript" type="text/javascript" src="<?php echo $folder;?>js/select2.min.js"></script>
