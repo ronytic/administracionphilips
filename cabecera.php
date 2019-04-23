c986b222faee5dae877eca131ec66a3a76c4c760
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-04-23 15:03:30 -0400

    Update cabecera.php

--- cabecera.php
+++ cabecera.php
@@ -1,9 +1,9 @@
 </head>
 <body>
-<div class="container" > 
+<div class="container" >
 	<div class="col-lg-12">
     	<div class="banner">
-		<img src="<?php echo $folder;?>imagenes/cabecera2.jpg" alt="" width="100%" height="200">
+		<!-- <img src="<?php echo $folder;?>imagenes/cabecera2.jpg" alt="" width="100%" height="200"> -->
         </div>
 	</div>
 	<div class="clear"></div>
