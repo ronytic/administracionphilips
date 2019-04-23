421fce22026862ecd39342b229dc766f8ec30de6
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-04-23 18:57:07 -0400

    Update mensajeresultado.php

--- mensajeresultado.php
+++ mensajeresultado.php
@@ -1,46 +1,54 @@
 <?php
 include_once $folder.'cabecerahtml.php';
-if($archivonuevo=="" && empty($archivonuevo)){
-	$archivonuevo="nuevo.php";
-}
-if($archivovolver=="" && empty($archivovolver)){
-	$archivovolver="modificar.php";
-}
-if($archivolistar=="" && empty($archivolistar)){
-	$archivolistar="listar.php";
-}
+
+    if(empty($archivonuevo)){
+        $archivonuevo="nuevo.php";
+    }
+
+    if( empty($archivovolver)){
+        $archivovolver="modificar.php";
+    }
+
+    if(empty($archivolistar)){
+        $archivolistar="listar.php";
+    }
+
 
 ?>
 <?php include_once $folder.'cabecera.php';?>
-<div class="grid_12">
-	<div class="contenido imagenfondo">
-    	<div class="prefix_4 grid_4 alpha">
+<div class="col-lg-12">
+	<div class="row imagenfondo">
+    	<div class="col-lg-offset-4 col-lg-4 alpha">
 			<fieldset>
                 <?php
                 foreach($mensaje as $m){
 					?>
                     <div class="titulo"><?php echo $m?></div>
                     <?php
-						
+
 				}
 				?>
-                <?php if(count($botones)){foreach($botones as $ba=>$bn){
+                <?php if(isset($botones)){if(count($botones)){foreach($botones as $ba=>$bn){
 				?><a href="<?php echo $ba;?>?id=<?php echo $id;?>" class="botoninfo" target="_blank" ><?php echo $bn?></a><?php
-				}
+                }
+                 }
 				}?>
                 <hr />
-                <?php if($nuevo==0){?>
+                <?php if(isset($nuevo)){if($nuevo==0){?>
                 <a href="<?php echo $archivonuevo;?>" class="botoncorrecto" >Nuevo Registro</a>
-                <?php }?>
-                <?php if($codinsercion!=""){?>
+                <?php }
+                }?>
+                <?php if(isset($codinsercion)){if($codinsercion!=""){?>
                 <a href="<?php echo $archivovolver;?>?id=<?php echo $codinsercion;?>" class="botoninfo" >Modificar Registro Insertado</a>
-                <?php }?>
-                <?php if($listar==0){?>
+                <?php }
+                }?>
+                <?php if(isset($listar)){if($listar==0){?>
                 <a href="<?php echo $archivolistar;?>" class="botonalerta">Listar Registros</a>
-                <?php }?>
+                <?php }
+                }?>
          	</fieldset>
         </div>
         <div class="clear"></div>
     </div>
-</div> 
+</div>
 <?php include_once($folder."piepagina.php")?>
