9a7987c1566e89ada02addc4435c4c35ba5ba0ac
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-04-23 12:52:06 -0400

    Update login.php

--- login/login.php
+++ login/login.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-@set_magic_quotes_runtime(0);
 //header("Content-Type: text/html;charset=utf-8");
 if(!empty($_POST)){
 	include_once("../config.php");
@@ -11,14 +10,14 @@ if(!empty($_POST)){
 	$logusuarios=new logusuarios;
 	$url=$_POST['u'];
 	$u=explode($directory."/",$_POST['u']);
-	
+
 	if(isset($_POST['usuario'],$_POST['pass']) && $_POST['usuario']!="" && $_POST['pass']!=""){
 		$usuario=($_POST['usuario']);
 		$pass=$_POST['pass'];
 //		$usuario=str_replace("ñ","n",$usuario);
 		$usuarioAl=str_replace(array("ñ","Ñ"),array("n","N"),$usuario);
 		$usuarioAl=strtolower($usuarioAl);
-		
+
 	//	echo $usuarioAl;
 		$agente=$_SERVER['HTTP_USER_AGENT'];
 		$ip=$_SERVER['REMOTE_ADDR'];
@@ -26,22 +25,21 @@ if(!empty($_POST)){
 		$referencia= $_SERVER['HTTP_REFERER'];
 		$fecha=date("Y-m-d");
 		$hora=date("H:i:s");
-		if(ereg('^[a-z]*[a-z]$',$usuario)){
+		if(preg_match('/^[a-z]*[a-z]$/',$usuario)){
 			//Administrador 1 2 3 4
-			
 			$reg=$usuarios->loginUsuarios($usuario,$pass);
 			$reg=array_shift($reg);
 			$sw=1;
 		}else{
-			header("Location:./?u=".$url.'&error=1');		
+			header("Location:./?u=".$url.'&error=1');
 		}
 		$codUsuario=$reg['codusuarios'];
-		
+
 		if($sw){
 			$Nivel=$reg['nivel'];
 		}
-		
-		
+
+
 		if($reg['Can']==1){
 			$valuesLog=array(
 				"idusuario"=>$codUsuario,
@@ -55,12 +53,7 @@ if(!empty($_POST)){
 				"lenguaje"=>"'$lenguaje'"
 			);
 			$logusuarios->insertar($valuesLog,0);
-			session_register("idusuario");
-			session_register("login");
-			session_register("nivel");
-			session_register("horasesion");
-			session_register("idmenu");
-			session_register("subm");
+
 			$_SESSION['idusuario']=$codUsuario;
 			$_SESSION['login']=1;
 			$_SESSION['nivel']=$Nivel;
@@ -73,7 +66,7 @@ if(!empty($_POST)){
 		}
 		//mysql_close($l);
 	}else{
-		header("Location:./?u=".$url.'&incompleto=1');	
+		header("Location:./?u=".$url.'&incompleto=1');
 	}
 }
 ?>
